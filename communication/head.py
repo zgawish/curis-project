8494fbd6e3c590676d0e069ad06c352dc51752d8
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-08-05 03:02:26 -0700

    added head server file

--- communication/head.py
+++ communication/head.py
@@ -2,17 +2,21 @@ from server import InstanceServer
 from client import InstanceClient
 
 class HeadServer(InstanceServer):
-    children = []
+    children = [] # list() but can be any data structure like dict()
 
-    def __init__(self):
+    def __init__(self, port=self.PORT):
+        self.ADDR = (self.SERVER, port)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(self.ADDR)
         self.set_up_clients()
 
     def set_up_clients(self):
         with open("children.txt", "r") as file:
-            lines = file.read_lines()
+            ips = file.read_lines()
             count = 0 # just to read first line for now
-            for line in lines:
-                
+            for ip in ips:
+                if count == 1:
+                    break
+                child = InstanceClient(5050, ip)
+                count += 1
 
