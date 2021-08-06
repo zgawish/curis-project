60db289af5be82e26602d27b45bf5d4de94f2b5b
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-08-06 15:04:49 -0700

    small test fix in nf_test

--- nf_test.py
+++ nf_test.py
@@ -1,6 +1,6 @@
 import os
 path = "~/curis-project"
-stream = os.popen("{path}./nextflow run {path}client.nf").format(path=path)
+stream = os.popen("{path}./nextflow run {path}client.nf".format(path=path))
 output = stream.read()
 print(str(output))
 
