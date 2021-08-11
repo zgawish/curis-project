5da660c53841cfdf6b5feb5e41374da5c106663b
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-08-11 02:44:22 -0700

    updated server to show msg in file and updtated trial to stop instances

--- stop_instance.py
+++ stop_instance.py
@@ -30,9 +30,9 @@ def stop_instance(name, msg):
 
 def main():
     cmd = sys.argv[1]
-    ls = cmd.split(', \n')
-    msg = ls[0]
-    name = ls[1]
+    ls = cmd.split(',\n')
+    # msg = ls[0]
+    # name = ls[1]
     print(ls)
     # print(stop_instance(name, msg))
 
