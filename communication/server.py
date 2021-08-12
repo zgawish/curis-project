9203c5d87bd0b51290cd1d4a358521b1188e5d98
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-08-11 23:04:41 -0700

    added time stamp to output

--- communication/server.py
+++ communication/server.py
@@ -72,7 +72,9 @@ class InstanceServer:
             if msg != "":
                 from_who = str(addr[0]) + ": " + msg
                 print(from_who)
-                os.system("echo {} >> /home/ziygawish/curis-project/msg".format(from_who))
+                t = time.localtime()
+                current_time = time.strftime("%H:%M:%S", t)
+                os.system("echo '{}: {}' >> /home/ziygawish/curis-project/msg".format(current_time, from_who))
                 send_msg = self.parse_message(msg)
                 print(send_msg)
                 self.send(conn, send_msg)
