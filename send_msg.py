9644b59cffda9c7108d4669c80a4cb2b6d6152e2
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-08-22 17:30:44 -0700

    new demo changes

--- send_msg.py
+++ send_msg.py
@@ -1,7 +1,31 @@
+# import sys
+# from communication.client import InstanceClient
+
+# def send_msg(ip, msg): # 2
+#     client = InstanceClient(5060, ip)
+#     client.connect_client()
+#     r_msg = client.quick_send("Dear {}: Hello from 10.128.0.3!".format(ip)) # 1
+#     # r_msg = client.quick_send(msg) # 2
+#     return r_msg
+
+# def main():
+#     ip = sys.argv[1]
+#     msg = sys.argv[2] # 2
+#     r_msg = send_msg(ip, msg) # 2
+#     print(r_msg) # 1
+#     results = r_msg.split('\n')
+#     exit(int(results[-1])) # exits with status
+    
+
+
+
+# if __name__ == "__main__":
+#     main()
+
 import sys
 from communication.client import InstanceClient
 
-def send_msg(ip, msg): # 2
+def send_msg(ip):
     client = InstanceClient(5060, ip)
     client.connect_client()
     r_msg = client.quick_send("Dear {}: Hello from 10.128.0.3!".format(ip)) # 1
@@ -10,9 +34,8 @@ def send_msg(ip, msg): # 2
 
 def main():
     ip = sys.argv[1]
-    msg = sys.argv[2] # 2
-    r_msg = send_msg(ip, msg) # 2
-    print(r_msg) # 1
+    r_msg = send_msg(ip)
+    print(r_msg)
     results = r_msg.split('\n')
     exit(int(results[-1])) # exits with status
     
