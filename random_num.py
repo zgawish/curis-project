90fa634a60c45a5c88bb58f8bb734c57bfca3fdf
Ziyad Gawish <ziyad@DNa808647.SUNet>
2021-08-23 16:04:38 -0700

    random command

--- random_num.py
+++ random_num.py
@@ -1,6 +1,6 @@
 import random
 
 nums = [0, 1]
-code = random.choices(nums, weights=(30, 70), k=1)[0]
-print(code)
+code = str(random.choices(nums, weights=(30, 70), k=1)[0])
+print(code+ "\n")
 exit(code)
